227d6ec5aa391b0d14718a8e98d9ed9834ae51c5
AYYANAR VIGNESH <67584044+ayyanar-vignesh@users.noreply.github.com>
2024-12-19 21:24:22 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function login(){
+    console.log("yes login");
+    document.getElementById('demo').innerHTML="please login";
+    document.getElementById('demo1').innerHTML="please login";
+    document.getElementById('demo2').innerHTML="please login";
+
+}
+
+function register(){
+    console.log("hi register");
+}
